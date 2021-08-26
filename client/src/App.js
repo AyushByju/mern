b14aefa1ebe914ec292067334683e981590d94e0
AyushByju <87507065+AyushByju@users.noreply.github.com>
2021-08-26 16:34:06 -0400

    c2

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import {useState} from "react";  
 
 function App() {
   return (
     <div className="App">
+      <div className="inputs">
       <input type="text" />
       <input type="number" />
+      </div>
     </div>
   );
 }

6778f2bd9e7e248ea22795eec8066b0fa4744875
AyushByju <87507065+AyushByju@users.noreply.github.com>
2021-08-26 21:15:34 -0400

    c3

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,58 @@
 import './App.css';
-import {useState} from "react";  
+import {useState, useEffect } from "react";  
+import Axios from 'axios';
 
 function App() {
+const [name, setName] = useState("");
+const [age, setAge] = useState(0);
+const [listOFFriends, setListOfFriends] = useState([]);
+
+const addFriend = () => {
+  Axios.post('http://localhost:3001/addfriend',{
+    name: name, 
+    age: age,
+   }).then(()=>{
+    alert("it worked");
+  })
+  .catch(()=> {
+    alert("it didnt work");
+  });
+};
+useEffect(() => {
+  Axios.get('http://localhost:3001/read')
+  .then((response) => {
+    setListOfFriends(response.data)
+  })
+  .catch(()=> {
+    console.log("ERROR");
+  });
+}, []);
   return (
     <div className="App">
       <div className="inputs">
-      <input type="text" />
-      <input type="number" />
+      <input type="text" placeholder="Friend name..."  
+      onChange={(event)=> {
+        setName(event.target.value)
+      }}
+      />
+      <input type="number" 
+      placeholder="Friend age..."
+      onChange={(event)=> {
+        setAge(event.target.value)
+      }}
+      />
+
+      <button onClick={addFriend}>Add Friend</button>
       </div>
+
+      {listOFFriends.map((val) => {
+        return (
+        <div>
+          {" "}
+          {val.name} {val.age}
+           </div>
+      );
+        })}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const mongoose = require('mongoose');
 const FriendModel = require('./models/Friends')
+
+app.use(express.json())
+app.use(cors());
 
 mongoose.connect("mongodb://localhost:27017/Mern?readPreference=primary&appname=MongoDB%20Compass&ssl=false", 
 {useNewUrlParser: true}
 );
 
-app.get("/insert", async (req, res) => {
-    const friend = new FriendModel({ name: "Jessic", age: 38 });
+app.post("/addfriend", async (req, res) => {
+    const name = req.body.name;
+    const age = req.body.age;
+
+    const friend = new FriendModel({ name: name, age: age });
     await friend.save();
-    res.send("Inserted DATA");
+    res.send("Success");
   });
   
 
